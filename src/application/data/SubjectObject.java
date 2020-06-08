671b157a771f3bd0534da04b7789913ee515626a
Graef <graef.luca@fh-swf.de>
2020-06-08 13:12:45 +0200

    SHA 256 Hash fuer ID hinzugefügt, prototypen für Level, Subject, Vocab fertiggestellt

--- src/application/data/SubjectObject.java
+++ src/application/data/SubjectObject.java
@@ -1,16 +1,33 @@
 package application.data;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class SubjectObject {
 	private ArrayList<VocabObject> vocabList = new ArrayList<VocabObject>();
 	
-	public void addVocab(String vocab) {
-		VocabObject curr = new VocabObject(vocab, vocabList.size());
+	/**
+	 * Creates new vocab for this subject
+	 * @param vocab
+	 * @throws NoSuchAlgorithmException
+	 */
+	public void addVocab(String vocab) throws NoSuchAlgorithmException {
+		VocabObject curr = new VocabObject(vocab);
 		vocabList.add(curr);
 	}
 	
-	public void removeVocab(int id) {
-		
+	/**
+	 * Removes subject from this subject
+	 * @param id
+	 */
+	@SuppressWarnings("unlikely-arg-type")
+	public void removeVocab(byte[] id) {
+		Optional<VocabObject> vocab =vocabList.stream().filter(v -> v.getId().equals(id)).findFirst();
+		if(vocab == null || vocab.isEmpty()) {
+			throw new IllegalAccessError("Object does not exist!");
+		}else {
+			vocabList.remove(vocab);
+		}
 	}
 }
--- src/application/data/VocabObject.java
+++ src/application/data/VocabObject.java
@@ -1,33 +1,35 @@
 package application.data;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class VocabObject {
 	private String vocab;
-	
-	/**
-	 * TODO: Hash ID
-	 */
-	private int id;
+	private byte[] id;
 	private LevelObject level = new LevelObject();
-	
-	public VocabObject(String vocab, int id) {
+
+	public VocabObject(String vocab) throws NoSuchAlgorithmException {
 		this.vocab = vocab;
-		this.id = id;
+		this.id = getSHA(vocab);
 	}
-	
+
 	/**
 	 * Get vocab string
+	 * 
 	 * @return
 	 */
 	public String getVocab() {
 		return vocab;
 	}
-	
+
 	/**
 	 * Set vocab String
+	 * 
 	 * @param vocab
 	 */
 	public void setVocab(String vocab) {
-		if(vocab == null || vocab.isEmpty()) {
+		if (vocab == null || vocab.isEmpty()) {
 			throw new IllegalArgumentException();
 		}
 		this.vocab = vocab;
@@ -35,24 +37,38 @@ public class VocabObject {
 
 	/**
 	 * Get int id
+	 * 
 	 * @return
 	 */
-	public int getId() {
+	public byte[] getId() {
 		return id;
 	}
-	
+
 	/**
 	 * Returns current level
+	 * 
 	 * @return
 	 */
 	public short getLevel() {
 		return level.getCurrLevel();
 	}
-	
+
 	/**
 	 * Level up, if max level not reached
 	 */
 	public void levelUp() {
 		level.levelUp();
 	}
+	
+	/**
+	 * Generates hash for id
+	 * @param input
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	public byte[] getSHA(String input) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		return md.digest(input.getBytes(StandardCharsets.UTF_8));
+	}
+
 }
