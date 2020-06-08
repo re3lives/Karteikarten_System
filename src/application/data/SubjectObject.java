6e1d2d9ff82f9818d9e79b959a4710c6e733fbe2
Graef <graef.luca@fh-swf.de>
2020-06-08 13:31:26 +0200

    EditVocab und getVocabById hinzugefügt, andere Funktionen entsprechend angepasst

--- src/application/data/SubjectObject.java
+++ src/application/data/SubjectObject.java
@@ -13,21 +13,52 @@ public class SubjectObject {
 	 * @throws NoSuchAlgorithmException
 	 */
 	public void addVocab(String vocab) throws NoSuchAlgorithmException {
+		if(vocab == null || vocab.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		
 		VocabObject curr = new VocabObject(vocab);
+		if(vocabList.contains(curr)) {
+			throw new IllegalArgumentException("The element already exisits!");
+		}
 		vocabList.add(curr);
 	}
 	
 	/**
-	 * Removes subject from this subject
+	 * Removes vocab from this subject
 	 * @param id
 	 */
-	@SuppressWarnings("unlikely-arg-type")
-	public void removeVocab(byte[] id) {
-		Optional<VocabObject> vocab =vocabList.stream().filter(v -> v.getId().equals(id)).findFirst();
-		if(vocab == null || vocab.isEmpty()) {
-			throw new IllegalAccessError("Object does not exist!");
-		}else {
-			vocabList.remove(vocab);
+	public void removeVocab(String oldVocab) {
+		if(oldVocab == null || oldVocab.isEmpty()) {
+			throw new IllegalArgumentException();
 		}
+		VocabObject vocab = getVocabByString(oldVocab);
+		vocabList.remove(vocab);
+	}
+	
+	/**
+	 * Edits vocab from this subject
+	 * @param id
+	 * @param newVocab
+	 */
+	public void editVocab(String oldVocab, String newVocab) {
+		if(oldVocab == null || newVocab == null || oldVocab.isEmpty() ||newVocab.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		VocabObject vocab = getVocabByString(oldVocab);
+		vocab.setVocab(newVocab);
+	}
+	
+	/**
+	 * Returns vocab if existing
+	 * @param id
+	 * @return
+	 */
+	private VocabObject getVocabByString(String str) {
+		VocabObject vocab =  vocabList.stream().filter(v -> v.getVocab().equals(str)).findFirst().orElse(null);
+		if(vocab == null) {
+			throw new IllegalAccessError("Object does not exist!");
+		}
+		return vocab;
 	}
 }
