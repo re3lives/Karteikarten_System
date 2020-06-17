bca2a043b48df80d2d672a7f4114c9a0b4d93528
shidozu <uerpmann.renan@fh-swf.de>
2020-06-18 01:51:32 +0200

    Grobes Layout erstellt mit Logo und Beenden, Über uns Funktion implementiert.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,30 +3,39 @@ package application;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
+import application.data.SubjectManager;
+import application.data.SubjectObject;
+import application.data.VocabObject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
-	
-	
-	
+
 	@Override
 	public void start(Stage primaryStage) throws NoSuchAlgorithmException, IOException {
 
-		
+		SubjectManager manager = new SubjectManager();
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml")); 
-			
+			SubjectObject sub = manager.createSubject("Moin");
+			VocabObject vocab = sub.createVocab("hi");
+			System.out.println(vocab.getLevel());
+			vocab.levelDown();
+			System.out.println(vocab.getLevel());
+			vocab.levelUp();
+			System.out.println(vocab.getLevel());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+
 			Scene scene = new Scene(root, 1366, 768);
-			
+
 			primaryStage.setTitle("Vokabeltrainer");
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -1,15 +1,33 @@
 package application;
 
-import javafx.scene.control.Button;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainWindowController {
+
+	public void closeApp() {
+		Platform.exit();
+		System.exit(0);
+	}
 	
-	public Button button;
-	
-	
-	public void handleButtonClick()
-	{
-		button.setText("Error");
+	public void openAbout() {
+		
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("about.fxml"));
+
+			Scene scene = new Scene(root, 600, 300);
+
+			Stage stage = new Stage();
+			stage.setTitle("About");
+			stage.setScene(scene);  
+	        stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
